98092c3f5b69f0a879951c8f0fc665613710ee12
saya-dev <sametcetinkaya00s@gmail.com>
2023-12-16 14:07:45 +0300

    Popup & PurgeCSS setup.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './app.postcss';
 import App from './App.svelte';
 
+import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+import { storePopup } from '@skeletonlabs/skeleton';
+storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
 const app = new App({
    target: document.getElementById('app') as Element
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [svelte()]
+   plugins: [svelte(), purgeCss()]
 });
